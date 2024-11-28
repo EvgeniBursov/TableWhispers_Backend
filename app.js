e6b0997a2e3ab1939f5501cbbd777f13ae847ffe
Evgeni Bursov <94795773+EvgeniBursov@users.noreply.github.com>
2024-11-28 08:59:54 +0200

    add validation fuction

--- app.js
+++ app.js
@@ -3,19 +3,42 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 
-const app = express();
-app.use(bodyParser.json());
+const client_register_route = require('./routes/client_register_route')
+const client_login_route = require('./routes/client_login_route')
 
-const DATABASE_URL = "mongodb+srv://evgenbu2:xq8zmS4ABlsldbMa@webproject.fupstrj.mongodb.net/?authMechanism=DEFAULT"
-console.log('Connected to the DataBase successfully 1');
+const res_register_route = require('./routes/res_register_route')
+const res_login_route = require('./routes/res_login_route')
 
-const result = dotenv.config()
+const client_app = express();
+const res_app = express();
 
-mongoose.connect(DATABASE_URL)
+client_app.use(bodyParser.json());
+client_app.use(client_register_route);
+client_app.use(client_login_route);
+
+res_app.use(bodyParser.json());
+res_app.use(res_register_route);
+res_app.use(res_login_route);
+
+dotenv.config()
+
+const client_app_port = process.env.CLIEN_PORT;
+const res_app_port = process.env.RES_PORT;
+
+client_app.listen(client_app_port, () => {
+    console.log('client server is up and running ', client_app_port);
+  });
+
+res_app.listen(res_app_port, () => {
+    console.log('res server is up and running ', res_app_port);
+  });
+
+
+const dataBaseURL = process.env.DATABASE_URL;
+mongoose.connect(dataBaseURL)
 const db = mongoose.connection
+
 db.on('error', error=>{console.log(error)})
 db.once('open',()=>{console.log('connected to mongo DB')})
 
-
-
-module.exports = app;
+module.exports = client_app, res_app;
--- controllers/client_register_controller.js
+++ controllers/client_register_controller.js
@@ -10,8 +10,23 @@ const createNewUser = async (req, res) => {
   var req_age = req.body.age;
   var req_phone_number = req.body.phone_number;
   var req_pass = req.body.password;
-  
-  try{
+  var req_confirm_pass = req.body.confirm_password;
+
+  const check = validatRegister(req_email,req_first_name,req_last_name,req_age,req_phone_number,req_pass,req_confirm_pass)
+
+  if (check.errors !== null) {
+    return res.status(400).json({
+        success: false,
+        errors: check.errors,
+        message: check.message,
+    });
+}
+
+if (req_confirm_pass!== req_pass) {
+  return res.status(400).json({ 'error': 'the passwords is not same' })
+}
+
+ try{
     const user = await ClientUser.findOne({'email': req_email})
     if(user != null){
       return res.json({ 'error': 'the user is exist' });
@@ -38,6 +53,59 @@ try{
 }
 }
 
+
+
+function validatRegister(email,first_name,last_name,age,phone_number,password,confirm_password) {
+  
+  const nameRegex = /^[a-zA-Z]{2,}$/; // Minimum 2 letters, only alphabets
+  const ageMin = 16; // Minimum age
+  const phoneRegex = /^05\d{8}$/; // 10 digits, starting with 05
+  const emailRegex = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/; // Basic email format
+  const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
+
+  /*
+  const isFirstNameValid = nameRegex.test(firstName);
+  const isLastNameValid = nameRegex.test(lastName);
+  const isAgeValid = age >= ageMin;
+  const isPhoneNumberValid = phoneRegex.test(phoneNumber);
+  const isEmailValid = emailRegex.test(email);
+  const isPasswordValid = passwordRegex.test(password);
+  */
+  const errors = {};
+
+    if (!nameRegex.test(first_name)) {
+        errors.firstName = "First name must have at least 2 letters.";
+    }
+    if (!nameRegex.test(last_name)) {
+        errors.lastName = "Last name must have at least 2 letters.";
+    }
+    if (age < ageMin) {
+        errors.age = "Age must be 16 or older.";
+    }
+    if (!phoneRegex.test(phone_number)) {
+        errors.phoneNumber = "Phone number must be 10 digits and start with 05.";
+    }
+    if (!emailRegex.test(email)) {
+        errors.email = "Email must be in the format name@name.com.";
+    }
+    if (!passwordRegex.test(password)) {
+        errors.password =
+            "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, 1 number, and be at least 6 characters long.";
+    }
+    if (!passwordRegex.test(confirm_password)) {
+      errors.password =
+          "Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 special character, 1 number, and be at least 6 characters long.";
+  }
+
+    return {
+        success: Object.keys(errors).length === 0,
+        errors: Object.keys(errors).length === 0 ? null : errors,
+        message:
+            Object.keys(errors).length === 0
+                ? "Validation successful."
+                : "Validation failed. Check the errors for more details.",
+    };
+}
 
 
   module.exports = {
